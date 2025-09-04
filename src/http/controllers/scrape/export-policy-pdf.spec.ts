d8c874327d9fb20e4686d80f15b7a10e8d7f2f8d
Luan <luaan.carlos@hotmail.com>
2025-09-04 00:29:56 -0300

    hutfix: tentando subir banco

--- src/http/controllers/scrape/export-policy-pdf.spec.ts
+++ src/http/controllers/scrape/export-policy-pdf.spec.ts
@@ -1,4 +1,4 @@
-import { app } from '@/app';
+ import { app } from '@/app';
 import { createPolicyPrismaTest } from '@/utils/test/createPolicyPrisma';
 import request from 'supertest';
 import { afterAll, beforeAll, describe, expect, it } from 'vitest';
